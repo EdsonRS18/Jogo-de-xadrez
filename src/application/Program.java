154ae115fe5ccc8d367268185494d46999939976
Edson Gomes <edson.bol.santos@hotmail.com>
2023-05-05 21:26:31 -0300

    melhorando a promocao

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,8 +39,12 @@ public class Program {
                 
 				if (xadrezPartida.getPromocao() != null) {
 					System.out.print("Entre com a promocao (B/C/T/Q): ");
-					String type = sc.nextLine();
-					xadrezPartida.substituirPeca(type);
+					String type = sc.nextLine().toUpperCase();
+                    while (!type.equals("B") && !type.equals("C") && !type.equals("T") & !type.equals("Q")){
+                        System.out.print("Entada invalida. Entre com a promocao (B/C/T/Q): ");
+                        type = sc.nextLine().toUpperCase();
+                    }
+                    xadrezPartida.substituirPeca(type);
 				}
                 
             }catch(XadrezException e){
--- src/xadrezCamada/XadrezPartida.java
+++ src/xadrezCamada/XadrezPartida.java
@@ -3,9 +3,6 @@ package xadrezCamada;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.security.InvalidParameterException;
-
-
 import tabuleiroCamada.Peca;
 import tabuleiroCamada.Posicao;
 import tabuleiroCamada.Tabuleiro;
@@ -134,7 +131,7 @@ public class XadrezPartida {
 			throw new IllegalStateException("não tem peca para ser promovida");
 		}
 		if (!type.equals("B") && !type.equals("C") && !type.equals("T") & !type.equals("Q")) {
-			throw new InvalidParameterException("Invalid type for promotion");
+			return promocao;
 		}
 
 		Posicao pos = promocao.getXadrezPosicao().toPosicao();
